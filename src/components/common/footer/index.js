c53644c71930d93cabc8444377a0d999a1395249
Akku83344 <akku83344@gmail.com>
2022-11-17 16:43:30 +0530

    second

--- src/components/common/footer/index.js
+++ src/components/common/footer/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='absolute-center'>Made with 🧡 by akku & oyush</div>
+    <div className='absolute-center'>Made with 🧡 by akku </div>
   )
 }
 
